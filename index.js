0a0845bc18f35204dc2cb17bf761153be9c05bb2
Jeremy Kaplan <jeremykaplan10@gmail.com>
2023-04-10 17:11:15 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42 - someValue)
+}
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+    
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceFare = distanceTravelledInFeet(start, destination)
+    if (distanceFare < 400) {
+        return 0;
+    } else if (distanceFare > 400 && distanceFare < 2000) {
+        return (distanceFare-400) * 0.02
+    } else if (distanceFare > 2000 && distanceFare < 2500) {
+        return 25;
+    } else if (distanceFare > 2500) {
+        return 'cannot travel that far'
+    }
+}
